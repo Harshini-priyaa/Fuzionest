eb4bc31ac8c664a6b8ba640220373f16d409eb91
Harshini-priyaa <harshinipriyaa1112@gmail.com>
2024-08-22 12:22:09 +0530

    date-format

--- my_timesheets/src/components/Timesheet.js
+++ my_timesheets/src/components/Timesheet.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import './Timesheet.css';
 
-const Timesheet = ({ image, title, dateRange, submittedHours, approvedHours }) => {
+const formatDate = (date) => {
+  const options = { day: '2-digit', month: 'short', year: 'numeric' };
+  const formattedDate = new Intl.DateTimeFormat('en-GB', options).format(date).replace(',', '-').replace(' ', '-');
+  return formattedDate;
+};
+
+const Timesheet = ({ image, title, dateRange, submittedHours, approvedHours, count }) => {
+  const startDate = formatDate(new Date(dateRange.split(' - ')[0]));
+  const endDate = formatDate(new Date(dateRange.split(' - ')[1]));
+
   return (
-    <div className="timesheet">
-      <div className="timesheet-status">
-        <span className="status-icon">✅</span>
+    <div className="timesheet-wrapper">
+      <div className="timesheet-count">
+        <span>{count}</span>
       </div>
-      <div className="timesheet-image">
-        <img src={image} alt="User" />
-      </div>
-      <div className="timesheet-details">
-        <div className="timesheet-title">{title}</div>
-      </div>
-      <div className="timesheet-column">
-        <div>{dateRange}</div>
-        
-      </div>
-      <div className="timesheet-column">
-        <div>{submittedHours}</div>
-        <div>Submitted Hours</div>
-      </div>
-      <div className="timesheet-column">
-        <div>{approvedHours}</div>
-        <div>Approved Hours</div>
+      <div className="timesheet">
+        <div className="timesheet-status">
+          <span className="status-icon">✅</span>
+        </div>
+        <div className="timesheet-image">
+          <img src={image} alt="User" />
+        </div>
+        <div className="timesheet-details">
+          <div className="timesheet-title">{title}</div>
+        </div>
+        <div className="timesheet-column">
+          <div>{`${startDate} - ${endDate}`}</div>
+        </div>
+        <div className="timesheet-column">
+          <div>{submittedHours}</div>
+          <div>Submitted Hours</div>
+        </div>
+        <div className="timesheet-column">
+          <div>{approvedHours}</div>
+          <div>Approved Hours</div>
+        </div>
       </div>
     </div>
   );
